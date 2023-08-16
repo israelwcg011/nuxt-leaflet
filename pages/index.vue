78508c8192a51461848eaa9afd5a9c4a3e3aabef
Israel Oliveira <israel.z1@hotmail.com>
2023-08-16 08:47:26 -0300

    Modifications on files index.vue and in the leaflet plugin.

--- pages/index.vue
+++ pages/index.vue
@@ -1,82 +1,214 @@
 <template>
   <div id="map-wrap">
-    <rootMap :zoom="rootMapProps.zoom" :center="rootMapProps.center">
-      <baseMap v-for="baseMap in baseLayers" :key="baseMap.name" :url="baseMap.url" :attribution="baseMap.attribution">
-      </baseMap>
-      <!-- <l-wms-tile-layer v-for="wmsLayer in wmsLayers" :key="wmsLayer.name" :base-url="wmsLayer.url"
-        :layers="wmsLayer.layers" :visible="wmsLayer.visible" :name="wmsLayer.name" :attribution="wmsLayer.attribution"
-        :transparent="true" format="image/png" layer-type="base">
-      </l-wms-tile-layer> -->
-      <l-polygon
-      v-for="imovel in imoveisCar.features"
-      :key="imovel.id"
-      :lat-lngs="swapListOfArrays(imovel.geometry.coordinates)"
-      ></l-polygon>
-    </rootMap>
+    <!-- Overlays -->
+    <client-only>
+      <l-map
+        :zoom="rootMapProps.zoom"
+        :center="rootMapProps.center"
+        :options="rootMapProps.options"
+      >
+        <l-control position="topleft" class="l-control">
+          <v-app-bar-nav-icon
+            color="black"
+            @click.stop="drawer = !drawer"
+          ></v-app-bar-nav-icon>
+        </l-control>
+        <l-tile-layer
+          v-if="baseMapsGroup == 0"
+          :url="baseMap.url"
+          :attribution="baseMap.attribution"
+        ></l-tile-layer>
+        <l-wms-tile-layer
+          v-if="baseMapsGroup == 1"
+          :base-url="srtm30.url"
+          :layers="srtm30.layers"
+        ></l-wms-tile-layer>
+        <l-tile-layer
+          v-if="baseMapsGroup == 2"
+          :url="worldImagery.url"
+          :attribution="worldImagery.attribution"
+        ></l-tile-layer>
+        <l-geo-json
+          v-if="layersActive.includes(0)"
+          :geojson="geojson"
+          :options="options"
+        ></l-geo-json>
+      </l-map>
+    </client-only>
+
+    <!-- Navigation drawer -->
+    <v-navigation-drawer class="nav" v-model="drawer" absolute bottom temporary>
+      <v-list nav dense>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6"> Mapas base </v-list-item-title>
+        </v-list-item-content>
+        <v-list-item-group
+          v-model="baseMapsGroup"
+          active-class="deep-purple text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-title>Open Street</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>SRTM 30 Colored Hillshade</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>World Imagery</v-list-item-title>
+          </v-list-item>
+
+          <!-- <v-list-item>
+            <v-list-item-title>Fizz</v-list-item-title>
+          </v-list-item> -->
+        </v-list-item-group>
+      </v-list>
+      <v-divider></v-divider>
+      <v-list nav dense>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6"> Camadas </v-list-item-title>
+        </v-list-item-content>
+
+        <v-list-item-group
+          v-model="layersActive"
+          multiple
+          active-class="deep-purple text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-title>Pays de la Loire</v-list-item-title>
+          </v-list-item>
+
+          <!-- <v-list-item>
+            <v-list-item-title>Bar</v-list-item-title>
+          </v-list-item>
+         -->
+        </v-list-item-group>
+      </v-list>
+      <v-divider></v-divider>
+      <v-list nav dense>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            Pesquisar coordenada
+          </v-list-item-title>
+        </v-list-item-content>
+        <v-list-item-content>
+          <v-text-field
+            v-model="coordinates"
+            label="latitude, longitude"
+            solo
+            @keydown.enter="findCoordinate"
+          ></v-text-field>
+        </v-list-item-content>
+      </v-list>
+    </v-navigation-drawer>
   </div>
 </template>
 <script>
-import baseLayers from "../assets/data/baselayers.json";
-import wmsLayers from "../assets/data/wmslayers.json";
-
 export default {
   data() {
     return {
-      model: 1,
-      drawer: true,
-      mini: true,
-      imoveisCar: null,
-      items: [
-        { title: "Home", icon: "mdi-home-city" },
-        { title: "My Account", icon: "mdi-account" },
-        { title: "Users", icon: "mdi-account-group-outline" },
-      ],
+      drawer: false,
+      baseMapsGroup: 0,
+      enableTooltip: true,
+      baseMapActive: null,
+      layersActive: [],
+      geojson: null,
+      coordinates: "",
       rootMapProps: {
-        zoom: 7,
-        center: [2.705780, -60.881007],
+        zoom: 9,
+        center: [48.11788, -0.52042],
+        options: { zoomControl: false },
       },
-      baseLayers: [
-        {
-          name: "Open Street",
-          url: "http://{s}.tile.osm.org/{z}/{x}/{y}.png",
-          attribution: "teste",
-        },
-      ],
-      wmsLayers: [
-        {
-          url: "http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi",
-          name: "Weather Data",
-          visible: true,
-          format: "image/png",
-          layers: "nexrad-n0r-900913",
-          transparent: true,
-          attribution:
-            "Weather data &copy; 2012 <a href='https://mesonet.agron.iastate.edu/docs/nexrad_mosaic/'>IEM Nexrad</a>",
-        },
-      ],
+      baseMap: {
+        name: "Open Street",
+        url: "http://{s}.tile.osm.org/{z}/{x}/{y}.png",
+        attribution:
+          '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      },
+      srtm30: {
+        url: "http://ows.mundialis.de/services/service?",
+        layers: "SRTM30-Colored-Hillshade",
+      },
+      worldImagery: {
+        name: "World Imagery",
+        url: "https://tile.openstreetmap.org/%7Bz%7D/%7Bx%7D/%7By%7D.png",
+        attribution:
+          "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community",
+      },
     };
   },
-  async fetch() {
-    this.imoveisCar = await this.$axios.$get('http://localhost:8080/geoserver/gisdb/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=gisdb%3Aimoveis_sigef&maxFeatures=50&outputFormat=application%2Fjson')
+  computed: {
+    options() {
+      return {
+        onEachFeature: this.onEachFeatureFunction,
+      };
+    },
+    onEachFeatureFunction() {
+      if (!this.enableTooltip) {
+        return () => {};
+      }
+      return (feature, layer) => {
+        layer.bindTooltip(
+          "<div>code:" +
+            feature.properties.code +
+            "</div><div>nom: " +
+            feature.properties.nom +
+            "</div>",
+          { permanent: false, sticky: true }
+        );
+      };
+    },
+  },
+  watch: {
+    layersActive() {
+      console.log(this.layersActive);
+    },
+    baseMapsGroup() {
+      console.log(this.baseMapsGroup);
+    },
+  },
+  async created() {
+    const response = await fetch(
+      "https://rawgit.com/gregoiredavid/france-geojson/master/regions/pays-de-la-loire/communes-pays-de-la-loire.geojson"
+    );
+    const data = await response.json();
+    this.geojson = data;
   },
   methods: {
-    swapListOfArrays(listOfLists){
-      return listOfLists.map(([x0, x1]) => {
-        return [x1, x0]
-      })
-    }
+    findCoordinate(event) {
+      event.preventDefault();
+      try {
+        let coordinatesParsed = this.coordinates.replace(" ", "");
+        let [latitude, longitude] = coordinatesParsed.split(",");
+        latitude = parseFloat(latitude);
+        longitude = parseFloat(longitude);
+
+        if (latitude && longitude) {
+          if (latitude >= -180 && latitude <= 180) {
+            if (longitude >= -90 && longitude <= 90) {
+              this.rootMapProps.center = [latitude, longitude];
+              this.rootMapProps.zoom = 14;
+            }
+          }
+        } else {
+          console.log("Something went wrong.");
+        }
+        console.log(latitude);
+        console.log(longitude);
+      } catch (error) {
+        console.log(error);
+      }
+    },
   },
-  mounted() {
-    console.log(this.swapListOfArrays([
-      ['a', 'b'],
-      ['c', 'd']
-    ]))
-  },
+  mounted() {},
 };
 </script>
 <style>
 #map-wrap {
   height: 100%;
   width: 100%;
+}
+.nav {
+  z-index: 1000;
 }
 </style>
--- plugins/leaflet.js
+++ plugins/leaflet.js
@@ -1,8 +1,11 @@
 import Vue from 'vue';
-import { LMap, LTileLayer, LMarker, LWMSTileLayer, LPolygon } from 'vue2-leaflet';
+import { LMap, LTileLayer, LMarker, LWMSTileLayer, LPolygon, LGeoJson, LFeatureGroup, LTooltip } from 'vue2-leaflet';
 
 Vue.component('l-map', LMap);
 Vue.component('l-tile-layer', LTileLayer);
 Vue.component('l-marker', LMarker);
 Vue.component('l-wms-tile-layer', LWMSTileLayer);
-Vue.component('l-polygon', LPolygon)
+Vue.component('l-polygon', LPolygon);
+Vue.component('l-geo-json', LGeoJson);
+Vue.component('l-feature-group', LFeatureGroup);
+Vue.component('l-tooltip', LTooltip);
